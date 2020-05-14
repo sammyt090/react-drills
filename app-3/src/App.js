e39a252110a044df15bb6a6f8cd92e0cf63b834f
sammyt090 <samueltanner231@gmail.com>
2020-05-13 19:19:12 -0700

    app-3-final

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class List extends Component{
+  constructor(){
+    super();
+    
+
+    this.state= {
+      movies: ["Star Wars", "Avengers", "Harry Potter", "The Lord of The Rings"],
+      filterMovies: ""
+    }
+  }
+
+  handleChange(filter) {
+    this.setState({filterMovies: filter})
+  }
+
+render(){
+  let favMovies = this.state.movies
+  .filter((e, i) => {
+    return e.includes(this.state.filterMovies);
+  })
+   .map((e, i) => <h2 key={i}>{e}</h2>);
+
+  
+      
+    return (
+    <div className ="List">
+      <input onChange={e=> this.handleChange(e.target.value)} type="text"/>
+      {favMovies}
+   </div>
+       );
+}
 }
 
-export default App;
+export default List;
