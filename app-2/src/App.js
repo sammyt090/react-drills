795f5680d6ff16c4bdf5019d230aad5097f5d412
sammyt090 <samueltanner231@gmail.com>
2020-05-13 18:47:16 -0700

    app-2-final

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class List extends Component{
+  constructor(){
+    super();
+    
+
+    this.state= {
+      movies: ["Star Wars", "Avengers", "Harry Potter", "The Lord of The Rings"]
+    }
+  }
+
+render(){
+  let favMovies = this.state.movies.map((e, i) => <h2 key={i}>{e}</h2>);
+
+  
+      
+    return <div className="List">{favMovies}</div>;
+}
 }
 
-export default App;
+export default List;
